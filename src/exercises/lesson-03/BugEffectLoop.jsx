43950247498bf1a9ba92c5fef043d090c6d14596
AnnnAPr <programming.aap@gmail.com>
2026-07-01 16:06:16 -1000

    add components to app

--- src/exercises/lesson-03/BugEffectLoop.jsx
+++ src/exercises/lesson-03/BugEffectLoop.jsx
@@ -14,9 +14,8 @@ export default function BugEffectLoop() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    setCount(count + 1);
+    setCount((prev) => prev + 1);
   }, []);
-
   return <p>Bug 1 Count: {count}</p>;
 }
 

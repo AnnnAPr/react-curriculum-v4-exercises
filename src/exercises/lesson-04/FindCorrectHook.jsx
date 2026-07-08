d3db07e894673d6258a2c9a4b8447d131724c119
AnnnAPr <programming.aap@gmail.com>
2026-07-07 15:19:33 -1000

    Update correct hook task

--- src/exercises/lesson-04/FindCorrectHook.jsx
+++ src/exercises/lesson-04/FindCorrectHook.jsx
@@ -1,21 +1,34 @@
 // TOPIC: Choose the correct tool: useRef vs useState
 // TASK: Make sure it updates the text *without* triggering a re-render
-import { useRef, useState } from 'react';
+
+// Use useRef hook to avoid re-renders
+import { useRef } from 'react';
 export default function FindCorrectHook() {
-  // let clickCount = 0; // ← incorrect implementation
-  // const clickCount = useRef(0);
-  const [clickCount, setClickCount] = useState(0);
+  // Track the number of clicks in background
+  const clickCount = useRef(0);
+
+  // Hold direct reference to the HTML button element in the DOM
+  const buttonRef = useRef(null);
 
   function handleClick() {
-    // clickCount++;
-    // clickCount.current++;
-    setClickCount(clickCount + 1);
+    // Increment the click count
+    clickCount.current++;
+
+    // Directly update browser DOM element
+    if (buttonRef.current) {
+      buttonRef.current.textContent = `${clickCount.current} Clicks`;
+    }
   }
 
   return (
     <div>
       <h2>useRef vs useState Decision</h2>
-      <button onClick={handleClick}>{clickCount} Clicks</button>
+      <button
+        ref={buttonRef} // react link DOM button element to buttonRef
+        onClick={handleClick}
+      >
+        0 Clicks
+      </button>
     </div>
   );
 }
